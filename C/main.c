2b70618bde4936b5cafd2d2e55c5da9abf318335
Gixu572 <gautam20219102@gmail.com>
2022-04-20 14:36:10 +0530

    has started learning C with SoloLearn :)

---
+++ C/main.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+
+int main()
+{
+    // printf("Enter a string : ");
+    // char a = getchar();
+
+    // gets(str);
+    // scanf("%s",&a);
+    // printf("You entered: ");
+    // putchar(a);
+    // int a, b;
+    // a = 10;
+    // b = 3;
+
+    // printf("%d\n",10/3);
+    // printf("%d\n",10%3);
+
+    // float price = 6.50;
+    // int increase = 2;
+    // float new_price;
+    // new_price = price + increase;
+
+    // printf("New price is %4.2f \n", new_price);
+
+    // int a =8;
+    // a++;
+    // printf("%d\n",a);
+    // int b = 7;
+    // printf("a: %d\n",a);
+    // printf("b:%d \n",b);
+    // a+=b--;
+    // a=a+b;
+    // printf("b:%d \n",b);
+    // printf("a:%d \n",a);
+
+    // int x = 3;
+    // int y = 8;
+    // if (!(x > 2 || y < 0))
+    //     printf("true");
+    // else
+    //     printf("false");
+
+    // int name = 10;
+    // char checkname=(name==10)?printf("equal \n") :printf("Not Equal \n");
+    // int a = 1;
+    // do
+    // {
+    //     printf("%d \n",a);
+    //     a++;
+
+    // }while (a<10);
+    // while (a<10){
+    //     printf("%d \n",a);
+    //     a++;
+    // }
+    // for (int i = 1; i > 0; i++)
+    // {
+    //     printf("%d \n",i);
+    //     if (i>10)
+    //     {
+    //         break;
+    //     }
+        
+    // }
+    // for(int i=0;i<5;i++){
+    //     printf("%d\n",i);
+    // }
+    int a = 10;
+    int x = 20;
+    int y = sum(a,x);
+    printf("%d\n",y);
+    
+
+
+    return 0;
+}
+int sum(int a, int b){
+    int s = a + b; 
+    return s;
+
+}
