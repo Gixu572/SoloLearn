cd1246a8222313dc2f1ea53145819818228ee982
Gixu572 <gautam20219102@gmail.com>
2022-04-26 23:19:23 +0530

    Added a few changes

--- C/POINTERS.C
+++ C/POINTERS.C
@@ -1,13 +1,28 @@
 #include <stdio.h>
 
+// int add_up(int *a, int num_elements);
 int main()
 {
-    int j = 46;
-    int *p= NULL;
-    p = &j;
-    printf("The adress of j : %x\n",&j );
-    printf("The adress of j in p : %x\n",p );
-    printf("The adress of j  : %d\n",j );
-    printf("The adress of *p %d\n",*p );
+    // int j = 46;
+    // int *p= NULL;
+    // p = &j;
+    // printf("The adress of j : %x\n",&j );
+    // printf("The adress of j in p : %x\n",p );
+    // printf("The adress of j  : %d\n",j );
+    // printf("The adress of *p %d\n",*p );
+
+    // int orders[5] = {100, 220, 37, 16, 98};
+
+    // printf("Total orders is %d\n", add_up(orders, 5));
     return 0;
 }
+// int add_up(int *a, int num_elements)
+// {
+//     int total = 0;
+//     int k;
+//     for (k = 0; k < num_elements; k++)
+//     {
+//         total = total + a[k];
+//     }
+//     return (total);
+// }
---
+++ README.md
@@ -0,0 +1,3 @@
+# Hi I am Gautam
+# This is my git repo where I post my code which I learn from Sololearn
+# Currently I am learning C 
